34715aeb01d59a30413f3da01eae872a368a27a3
hub-bla <hubertblaszczyk03@gmail.com>
2024-06-23 08:07:29 +0200

    emb request to python

--- backend/server/main.go
+++ backend/server/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
 	_ "github.com/lib/pq"
 )
+
+const EMB_SIZE int = 384
+
+type Embedding struct {
+	Data []float32 `json:"embedding"`
+}
 
 func main() {
 	const (
@@ -29,5 +39,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	test_message := []byte("{\"movie_title\": \"string\"}")
+	bodyReader := bytes.NewReader(test_message)
+	res, err := http.Post("http://127.0.0.1:8000/model/", "application/json", bodyReader)
+
+	if err != nil {
+		fmt.Println("something went wrong in making request")
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("something went wrong in reading body")
+	}
+
+	var titleEmbedding Embedding
+
+	json.Unmarshal(resBody, &titleEmbedding)
+
+	fmt.Printf("Response: %v", titleEmbedding.Data)
 
 }
