9aa032006280a0af6afb9611063fe3b110c015cc
hub-bla <hubertblaszczyk03@gmail.com>
2024-06-23 12:52:11 +0200

    add frontend config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Movie Recommender</h1>
+      <div className="card">
+        <input type='text' placeholder='Start searching :)'
+        onChange={() => setCount((count) => count +1)}/>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
