575756fb1e4ecd129a6a2d93c27ff651751c70cb
hub-bla <hubertblaszczyk03@gmail.com>
2024-06-26 21:10:44 +0200

    wait for image to load

--- frontend/src/modules/Book/Book.tsx
+++ frontend/src/modules/Book/Book.tsx
@@ -1,4 +1,6 @@
+import { useImage } from 'react-image'
 import './Book.css'
+import { Suspense } from 'react'
 interface BookProps {
     idx: number
     title: string
@@ -6,13 +8,29 @@ interface BookProps {
     findSimilarBooks: (id:number, title:string) => void
 }
 
+interface BookCoverProps {
+    img_path: string
+}
+
+
+
 const COVER_IMG_PREFIX = 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/'
+
+const BookCover: React.FC<BookCoverProps> = ({img_path}) =>{
+    const {src} = useImage({
+        'srcList': COVER_IMG_PREFIX+img_path
+    })
+    return <img src={src} alt=''/>
+}
+
 
 export const Book: React.FC<BookProps> = ({idx, title, img_path, findSimilarBooks}) => {
     return (
-        <div className='movie-component' onClick={() => findSimilarBooks(idx, title)}>
-            <h3>{title}</h3>
-            <img src={COVER_IMG_PREFIX+img_path} alt='' loading='lazy' />
-        </div>
+        <Suspense>
+            <div className='movie-component' onClick={() => findSimilarBooks(idx, title)}>
+                <h3>{title}</h3>
+                <BookCover img_path={img_path}/>
+            </div>
+        </Suspense>
     )
 }
